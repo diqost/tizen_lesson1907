a4b1fa54a34f403f1248ba868cb6eff29c09dd23
diqost <sapozhkov2008@yandex.ru>
2016-07-19 11:32:54 +0300

    video added

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,39 +1,59 @@
-var startTime;
-var checkTime;
+(function () {
+    var infoElem;
+    var imgElem;
 
-//Initialize function
-var init = function () {
-	// TODO:: Do your initialization job
-	console.log("init() called");
+    var init = function () {
+    	
+    	window.addEventListener( 'tizenhwkey', function( ev ) {
+            if( ev.keyName === "back" ) {
+                var activePopup = document.querySelector( '.ui-popup-active' ),
+                    page = document.getElementsByClassName( 'ui-page-active' )[0],
+                    pageid = page ? page.id : "";
 
-	// add eventListener for tizenhwkey
-	document.addEventListener('tizenhwkey', function(e) {
-		if(e.keyName == "back") {
-			try {
-				tizen.application.getCurrentApplication().exit();
-			} catch (error) {
-				console.error("getCurrentApplication(): " + error.message);
-			}
-		}
-	});
-};
-// window.onload can work without <body onload="">
-window.onload = init;
+                if( pageid === "one" && !activePopup ) {
+                    try {
+                        tizen.application.getCurrentApplication().exit();
+                    } catch (ignore) {
+                    }
+                } else {
+                    window.history.back();
+                }
+            }
+        } );
+    	
+    	// TODO:: Do your initialization job
+    	console.log("init() called");
 
-function startTime() {
-	var today = new Date();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	m = checkTime(m);
-	s = checkTime(s);
-	document.getElementById('divbutton1').innerHTML="Current time: " + h + ":" + m + ":" + s;
-	var t = setTimeout(startTime, 250);
-}
+    	// add eventListener for tizenhwkey
+    	document.addEventListener('tizenhwkey', function(e) {
+    		if(e.keyName == "back") {
+    			try {
+    				tizen.application.getCurrentApplication().exit();
+    			} catch (error) {
+    				console.error("getCurrentApplication(): " + error.message);
+    			}
+    		}
+    		
+    	});
+    	/*infoElem = document.getElementById('text1');
+        imgElem = document.getElementById('image1');
 
-function checkTime(i) {
-	if (i < 10) {
-		i="0" + i;
-	}
-	return i;
-}
+    	infoElem.innerHTML = screen.width + ":" + screen.height;*/
+    	
+    	
+    	$("#jqtxt").text("jQuery — библиотека JavaScript, фокусирующаяся на взаимодействии JavaScript и HTML. Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к атрибутам");
+     	$("#jqtxt").css('color', 'red');
+
+    };
+    // window.onload can work without <body onload="">
+    window.onload = init;
+    var state = false;
+
+    function changeImageVisibility() {
+    	state = ! state;
+    	
+    	imgElem.style.visibility = state ? 'hidden': 'visible';
+    	infoElem.style.visibility = state ? 'hidden': 'visible';
+
+    }
+} () );
